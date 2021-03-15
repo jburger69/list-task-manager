a8b3843aa89d01ba94afd4919801373286f67924
Joshua Burger <joshberger6969@outlook.com>
2021-03-15 15:00:22 -0400

    Nested task resources inside lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  root "lists#index"
 
 
+  resources :lists do
+    resources :tasks
+  end
 
-  resources :tasks
-  resources :lists
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
