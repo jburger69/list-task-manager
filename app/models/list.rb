8fa3adcde7f7517131d9e08cd7c57734eabba648
Joshua Burger <joshberger6969@outlook.com>
2021-03-15 15:12:26 -0400

    Created associations between models

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,6 @@
 class List < ApplicationRecord
+    has_many :tasks
+    has_many :user_lists
+    has_many :users, through: :user_lists
+    validates_presence_of :name, uniqueness :true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
 
       devise :omniauthable, :omniauth_providers => [:facebook]
 
+    has_many :user_lists
+    has_many :lists, through: :user_lists
+
     def self.from_omniauth(auth)
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
         user.email = auth.info.email
