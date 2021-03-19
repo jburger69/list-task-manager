fef655f3680061014a7677e45095f53671d94c19
Joshua Burger <joshberger6969@outlook.com>
2021-03-18 20:39:19 -0400

    New devise scope for logging a user out

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
