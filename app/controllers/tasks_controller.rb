1f7854a394b5c3f92bafc31cb8d58c9e4895453f
Joshua Burger <joshberger6969@outlook.com>
2021-03-18 21:45:44 -0400

    Created new Create for new List/1/tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,15 @@ class TasksController < ApplicationController
     def index
         @task = Task.all
     end
+
+    def create
+        @list = List.find(params[:list_id])
+        @task = @list.tasks.build(task_params)
+        @task.save
+        redirect_to @list
+    end
+
+    def task_params
+        params.require(:task).permit(:name, :status)
+    end
 end
