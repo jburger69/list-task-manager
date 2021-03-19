c28f20af08e81dd8c76ad113c316ec083cb201eb
Joshua Burger <joshberger6969@outlook.com>
2021-03-18 20:43:06 -0400

    Created Index, Show, New, Create and params for list controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,36 @@
 class ListsController < ApplicationController
 
     def index
+        @list = List.new
         @lists = List.all
+    end
+
+    def show
+        @list = List.find(params[:id])
+        @task = Task.new
+    end
+
+    def new
+        @list = List.new
+        @list.tasks.build
+    end
+
+    def create
+        @list = List.new(list_params)
+        if @list.save
+            redirect_to @list
+        else
+            render :new
+        end
+    end
+    
+    private
+
+    def list_params
+        params.require(:list).permit(:name, tasks_attributes: [:name, :status])
+    end
+
+    def require_login
+        return head(:forbidden) unless session.include? :user_id
     end
 end
