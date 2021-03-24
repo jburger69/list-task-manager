10d9754c631e9e095a3481d96bf043f4ff284ee7
Joshua Burger <joshberger6969@outlook.com>
2021-03-23 20:56:11 -0400

    Created a new show, Edit, and Destroy.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,8 @@ class TasksController < ApplicationController
     def index
         @task = Task.all
     end
+    def show
+    end
 
     def create
         @list = List.find(params[:list_id])
@@ -10,6 +12,21 @@ class TasksController < ApplicationController
         @task.save
         redirect_to @list
     end
+
+    def edit
+        @task = Task.find(params[:id])
+        @task.update(task_params)
+
+        redirect_to list_path(@task.list)
+    end
+
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        redirect_to list_path(@task.list)
+    end
+
+    private
 
     def task_params
         params.require(:task).permit(:name, :status)
