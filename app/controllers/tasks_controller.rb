47d533aeae3c752a3558534d85ef38d757e3982f
Joshua Burger <joshberger6969@outlook.com>
2021-03-25 22:05:00 -0400

    Created new validations and created new Update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,22 +3,39 @@ class TasksController < ApplicationController
     def index
         @task = Task.all
     end
+    
     def show
     end
 
     def create
-        @list = List.find(params[:list_id])
+        @list = current_user.lists.find(params[:list_id])
         @task = @list.tasks.build(task_params)
-        @task.save
-        redirect_to @list
+        if @task.valid?
+            @task.save
+            redirect_to @list
+        else
+            @error = @task.errors.full_messages
+            render :new
+        end
     end
 
     def edit
         @task = Task.find(params[:id])
-        @task.update(task_params)
-
-        redirect_to list_path(@task.list)
+        render :edit
     end
+
+    def update
+        @task = Task.find(params[:id])
+        @task.update(task_params)
+        if @task.valid?
+            @task.save
+            redirect_to list_path(@task.list)
+        else
+            @error = @task.errors.full_messages
+            render :edit
+        end
+    end
+    
 
     def destroy
         @task = Task.find(params[:id])
@@ -29,6 +46,6 @@ class TasksController < ApplicationController
     private
 
     def task_params
-        params.require(:task).permit(:name, :status)
+        params.require(:task).permit(:name)
     end
 end
