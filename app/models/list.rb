89d7cf7d98a9bb21c7782022de7b94aa14aa22f8
Joshua Burger <joshberger6969@outlook.com>
2021-11-10 09:14:52 -0500

    remove has_many users through user_lists

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,6 @@
 class List < ApplicationRecord
     has_many :tasks
     has_many :user_lists
-    has_many :users, through: :user_lists
     validates_presence_of :name
     validates :name, length: { minimum: 2 }, presence: true
 end
