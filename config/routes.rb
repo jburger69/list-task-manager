6789778988232fde577c08bc2b5f85d5c68766ec
Joshua Burger <joshberger6969@outlook.com>
2021-11-10 09:14:06 -0500

    oauth signout

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   root "lists#index"
 
 
+
   resources :lists do
     resources :tasks
   end
   
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
     devise_scope :users do
-      get '/users/sign_out' => 'devise/sessions#destroy' 
+      get '/users/sign_out' => 'devise/sessions#destroy'
     end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
