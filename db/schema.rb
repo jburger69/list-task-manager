e3fa3f795de16444b9dcbcf8874150a2d2d42d55
Joshua Burger <joshberger6969@outlook.com>
2021-11-10 09:13:36 -0500

    remove users table

--- db/schema.rb
+++ db/schema.rb
@@ -26,13 +26,6 @@ ActiveRecord::Schema.define(version: 2021_03_15_182848) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "user_lists", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "list_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
